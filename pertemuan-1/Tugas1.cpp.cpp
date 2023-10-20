89fef9285f2fe213f2b71d5d90408d1e87652b36
Dian <2310631170038@student.unsika.ac.id>
2023-10-20 21:51:43 +0700

    pertemuan-1

---
+++ pertemuan-1/Tugas1.cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout<<"-----------------------------------------------------------------------------------------------------------------------"<<endl;
+    cout<<"Nama : pangundian siagian"<<endl<<endl;
+    cout<<"NPM  : 2310631170038"<<endl<<endl;
+    cout<<"kelas: B - informatika"<<endl<<endl;
+    cout<<"cita - cita : pengusaha"<<endl<<endl;
+    cout<<"Motivasi masuk informatika : Linear dengan jurusan SMK,rekomendasi dari kaka kelas smk dan ada minat di bidang jaringan"<<endl<<endl;
+    cout<<"------------------------------------------------------------------------------------------------------------------------"<<endl;
+    return 0;
+}
+
